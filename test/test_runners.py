c7d01edb6230bec90e31a5e4d33775222f47462d
Nauja <jeremymorosi@hotmail.fr>
2021-03-16 10:48:45 +0100

    tests

--- test/test_runners.py
+++ test/test_runners.py
@@ -1,22 +1,22 @@
-__all__ = ["LoaderTestCase"]
+__all__ = ["RunnersTestCase"]
 import os
 import sys
 import yaml
 import unittest
-from gada import loader
+from gada import runners
 
 
-class LoaderTestCase(unittest.TestCase):
+class RunnersTestCase(unittest.TestCase):
     def test_load(self):
         """Test loading python runner."""
-        runner = loader.load_runner("python")
+        runner = runners.load("python")
 
         self.assertTrue(hasattr(runner, "Runner"), "invalid module")
 
     def test_load_not_found(self):
         """Test loading invalid runner."""
         with self.assertRaises(Exception):
-            loader.load_runner("invalid")
+            runners.load("invalid")
 
 
 if __name__ == "__main__":
