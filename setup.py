72227cc6a44201233aeee5de797573c6f2ddce60
Nauja <jeremymorosi@hotmail.fr>
2021-03-17 17:32:16 +0100

    fix python version

--- setup.py
+++ setup.py
@@ -44,13 +44,13 @@ setup(
     tests_require=["nose", "nose-cover3"],
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
